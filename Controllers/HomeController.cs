2f2942743593251964da936dd151e1fda6a96d8a
Robert Woods <robert.woods@fulbrightmail.org>
2022-11-01 08:16:17 -0400

    initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RedditLab.Models;
+using System.Diagnostics;
+
+namespace RedditLab.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        PRAPIDAL db = new PRAPIDAL();
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            Subreddit aww = db.GetSubreddit();
+
+            Child[] posts = db.GetSubData(aww);
+
+            return View(posts);
+        }
+
+        public IActionResult ViewPost(int Id)
+        {
+            Child post = db.GetPost(Id);
+
+            return View(post);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/PRAPIDAL.cs
@@ -0,0 +1,33 @@
+﻿using RestSharp;
+
+namespace RedditLab.Models
+{
+    public class PRAPIDAL
+    {
+        public Subreddit GetSubreddit()
+        {
+            var client = new RestClient("https://www.reddit.com/r/aww/.json");
+            var request = new RestRequest();
+            var response = client.GetAsync<Subreddit>(request);
+            Subreddit sr = response.Result;
+
+            return sr;
+        }
+
+        public Child[] GetSubData(Subreddit sub)
+        {
+            Data subData = sub.data;
+            Child[] postList = subData.children;
+
+            return postList;
+        }
+
+        public Child GetPost(int index)
+        {
+            Subreddit sub = GetSubreddit();
+            Child[] postList = GetSubData(sub);
+            Child post = postList[index];
+            return post;
+        }
+    }
+}
